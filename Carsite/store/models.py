5b0cf4797bcfd0c29b9dd3cf17eb7ec35de0b25f
Temirlan <umarov170604@gmail.com>
2024-04-18 11:34:35 +0000

    pagination жазылды

--- Carsite/store/models.py
+++ Carsite/store/models.py
@@ -15,6 +15,8 @@ class Car(models.Model):
     volume = models.DecimalField(max_digits=6, decimal_places=3)
     DESCRIPTION = models.TextField()
 
+    def __str__(self):
+        return f'{self.category} | {self.marka}'
 
 class Bet(models.Model):
     number = models.IntegerField()
@@ -22,3 +24,6 @@ class Bet(models.Model):
     buy_now = models.IntegerField()
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
+
+    def __int__(self):
+        return self.number
--- Carsite/store/views.py
+++ Carsite/store/views.py
@@ -1,16 +1,26 @@
 from rest_framework import viewsets, permissions
 from .models import *
 from .serializers import *
-# from .permissions import IsOwnerOrReadOnly
+from rest_framework.pagination import PageNumberPagination
+
+
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class CarViewSets(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permissions_classes = [permissions.IsAuthenticated]
+    pagination_class = WomenAPIListPagination
 
 
 class BetViewSets(viewsets.ModelViewSet):
     queryset = Bet.objects.all()
     serializer_class = BetSerializer
     permissions_classes = [permissions.AllowAny]
+    pagination_class = WomenAPIListPagination
+
+
